97c67c6dc7a1830424e24aa88b3fc431e316b733
JenSuper <253150284@qq.com>
2024-06-02 18:23:23 +0800

    add swagger

---
+++ initialize/router.go
@@ -0,0 +1,54 @@
+package initialize
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "internal_api/docs"
+	"internal_api/global"
+	"internal_api/middlewares"
+	"internal_api/router"
+	"net/http"
+)
+
+func Routers() *gin.Engine {
+	// 模式选择
+	gin.SetMode(chooseMode())
+
+	Router := gin.Default()
+	//配置跨域
+	Router.Use(middlewares.Cors())
+
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
+
+	// swagger http://localhost:17000/swagger/index.html
+	Router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	//ApiGroup := Router.Group("/api/v1")
+	ApiGroup := Router.Group("/api/v1", middlewares.JWTAuth())
+	// 服务初始化
+	router.InitAuthRouter(ApiGroup)
+	router.InitCommonRouter(ApiGroup)
+
+	return Router
+}
+
+// chooseMode 设置 gin 的启动模式
+func chooseMode() string {
+	switch global.ENV {
+	case "local":
+		return gin.DebugMode
+	case "test":
+		return gin.TestMode
+	case "prod":
+		return gin.ReleaseMode
+	default:
+		return gin.DebugMode
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"internal_api/global"
+	"internal_api/initialize"
+)
+
+// @title Internal API Server
+// @version 1.0
+// @description Internal API Server 服务
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Admin
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @BasePath
+// @securityDefinitions.apiKey ApiKeyAuth
+// @in header
+// @name Authorization
+func main() {
+
+	// 初始化 api
+	Router := initialize.Routers()
+
+	initialize.RedisClient()
+	initialize.MongoClient()
+
+	zap.S().Info(fmt.Sprintf("\naddr: http://localhost:%d \nswagger: http://localhost:%d/swagger/index.html", global.CONFIG.App.Port, global.CONFIG.App.Port))
+	// 启动 web 服务
+	if err := Router.Run(fmt.Sprintf(":%d", global.CONFIG.App.Port)); err != nil {
+		panic(fmt.Sprintf("启动失败:%s", err.Error()))
+	}
+
+}
