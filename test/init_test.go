2981e2a4a57b961c64e6785d6824e72a5fb522af
JenSuper <253150284@qq.com>
2025-02-26 11:37:53 +0800

    update test

--- test/init_test.go
+++ test/init_test.go
@@ -1,3 +1,38 @@
-func TestH(t *testing.T){
-	fmt.println(111)
+package test
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	"log"
+	"testing"
+)
+
+func TestH(t *testing.T) {
+	fmt.Println(111)
+}
+
+func TestFeed(t *testing.T) {
+	// 要订阅的 RSS Feed 地址
+	rssFeedURL := "https://rsshub.app/sina/csj"
+
+	// 创建一个新的 RSS 解析器
+	fp := gofeed.NewParser()
+
+	// 解析 RSS Feed
+	feed, err := fp.ParseURL(rssFeedURL)
+	if err != nil {
+		log.Fatal("Error parsing RSS Feed:", err)
+	}
+
+	// 打印 Feed 标题
+	fmt.Println("Feed Title:", feed.Title)
+
+	// 遍历所有的 Feed 项目并输出
+	for _, item := range feed.Items {
+		fmt.Printf("Title: %s\n", item.Title)
+		fmt.Printf("Link: %s\n", item.Link)
+		fmt.Printf("Published: %s\n", item.Published)
+		fmt.Println("Description:", item.Description)
+		fmt.Println("---------------")
+	}
 }
